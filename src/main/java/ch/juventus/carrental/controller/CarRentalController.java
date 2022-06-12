655f65fce3c563ab7f67ce631ff5d63420f951cc
luca-hom <luca.homberger@juventus.schule>
2022-06-12 17:36:04 +0200

    added Swagger annotations

--- src/main/java/ch/juventus/carrental/controller/CarRentalController.java
+++ src/main/java/ch/juventus/carrental/controller/CarRentalController.java
@@ -1,6 +1,13 @@
 package ch.juventus.carrental.controller;
 
 import ch.juventus.carrental.service.*;
+import com.fasterxml.jackson.core.type.TypeReference;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -8,6 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Objects;
 
 /**
@@ -25,7 +34,13 @@ public class CarRentalController {
         this.carRentalService = carRentalService;
     }
 
-
+    @Operation(summary = "Get a car by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the car",
+            content = { @Content(mediaType = "application/json",
+            schema = @Schema(implementation = Car.class))}),
+            @ApiResponse(responseCode = "404", description = "Car not found",
+            content = @Content)})
     @GetMapping("/api/v1/car/{id}")
     //http://localhost:8080/api/v1/car/124
     public ResponseEntity<String> carWithPathVariable(@PathVariable(value = "id") Long id) {
@@ -43,7 +58,15 @@ public class CarRentalController {
         return new ResponseEntity<String>(returnValue, HttpStatus.OK);
     }
 
-
+    @Operation(summary = "Replace a car by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the car",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Car.class))}),
+            @ApiResponse(responseCode = "404", description = "Car not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "Rental list of car to replace has to be empty",
+                    content = @Content)})
     @PutMapping( "/api/v1/car/{id}")
     public ResponseEntity<String> postCarWithPathVariable(@PathVariable(value = "id")Long id, @RequestBody Car replaceCar) {
         logger.info("PUT Request of car with id {}.", id);
@@ -61,7 +84,12 @@ public class CarRentalController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-
+    @Operation(summary = "Delete a car by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the car and deleted it",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Car not found",
+                    content = @Content)})
     @DeleteMapping( "/api/v1/car/{id}")
     public ResponseEntity<String> deleteCarWithPathVariable(@PathVariable(value = "id")Long id) {
         logger.info("DELETE Request of car with id {}.", id);
@@ -76,7 +104,11 @@ public class CarRentalController {
     }
 
 
-
+    @Operation(summary = "Get all cars, if filter is applied filter the list")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the car list",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema (implementation = Car.class)))})})
     @GetMapping("/api/v1/cars")
     //http://localhost:8080/api/v1/cars?filter=1
     public ResponseEntity<String> carRentalWithRequestParam(@RequestParam(value = "filter", required = false) String filter) {
@@ -98,17 +130,26 @@ public class CarRentalController {
         return new ResponseEntity<String>(returnValue, HttpStatus.OK);
     }
 
-
+    @Operation(summary = "Add a new car to the list")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully added the car",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Car.class))})})
     @PostMapping("/api/v1/car")
     public void registerNewCar(@RequestBody Car newCar) {
         logger.info("POST Request of a new car");
 
         carRentalService.createNewCar(newCar);
         logger.info("Successful POST Request of a new car");
-
     }
 
-
+    @Operation(summary = "Add a new rental to a car")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully added the Rental",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Rental.class))}),
+            @ApiResponse(responseCode = "400", description = "Car with this id not found",
+                    content = @Content)})
     @PostMapping("api/v1/car/{id}/rental")
     public ResponseEntity<Boolean> registerNewRental(@PathVariable (value = "id") Long id, @RequestBody Rental newRental) {
         logger.info("POST Request of a new rental for car with id {}", id);
